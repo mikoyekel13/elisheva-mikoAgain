bf38145683910074924b3144ee7be01c8431c457
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-21 15:45:26 +0200

    initial route structure

--- camels2/src/App.jsx
+++ camels2/src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<Layout />}>
+          <Route path="/home/:id" element={<Home />}>
+            <Route path="info/" element={<Info />} />
+            <Route path="posts/" element={<Posts />} />
+            <Route path="posts/:id" element={<Post />}>
+              <Route path="comments" element={<Comments />} />
+            </Route>
+            <Route path="albums/" element={<Albums />} />
+            <Route path="album/:id" element={<Album />}>
+              <Route path="photos" element={<Photos />} />
+            </Route>
+            <Route path="todos/" element={<Todos />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ camels2/src/assets/components/Nav.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/books">books</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
