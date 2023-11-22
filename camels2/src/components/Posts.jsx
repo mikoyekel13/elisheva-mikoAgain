b152c33a36be4dc7846e5d2803e858712102d52f
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-22 17:22:24 +0200

    delete,update and add posts

--- camels2/src/components/Posts.jsx
+++ camels2/src/components/Posts.jsx
@@ -7,12 +7,12 @@ const Posts = () => {
   const [showPost, setShowPost] = useState(false);
   const [showComments, setShowComments] = useState(false);
 
-  // const [hasShownPost, setHasShownPost] = useState(false);
-
   const fetchData = useFetch;
   const { id, postId } = useParams();
   const navigate = useNavigate();
-
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -31,58 +31,138 @@ const Posts = () => {
     fetchPosts();
   }, [showPost]);
 
-  const postsDisplay = posts.map((post) => {
-    return (
-      <div key={post.id}>
-        <button
-          onClick={() => {
-            setShowPost(true);
-            navigate(`/home/${id}/posts/${post.id}`);
-          }}
-        >
-          show post
-        </button>
-        <button
-          onClick={() => {
-            setShowPost(false);
-            navigate(`/home/${id}/posts`);
-          }}
-        >
-          hide post
-        </button>
-        <h4>id: {post?.id}</h4>
-        <h4>title: {post?.title}</h4>
-        {showPost && (
-          <>
-            <h4>userId: {post?.userId}</h4>
-            <h4>body: {post?.body}</h4>
-            <button
-              onClick={() => {
-                setShowComments(true);
-                navigate(`/home/${id}/posts/${post.id}/comments`);
-              }}
-            >
-              show comments
-            </button>
-            <button
-              onClick={() => {
-                setShowComments(false);
-                navigate(`/home/${id}/posts/${post.id}`);
-              }}
-            >
-              hide comments
-            </button>
-          </>
-        )}
-      </div>
+  async function deletePost(currPostId) {
+    await fetchData(`http://localhost:3000/posts/${currPostId}`, {
+      method: "DELETE",
+    });
+    setPosts((prevPosts) => {
+      let newPosts = [...prevPosts];
+      const postIndex = prevPosts.findIndex(
+        (post) => post.id === currPostId.id
+      );
+      newPosts.splice(postIndex, 1);
+      return newPosts;
+    });
+  }
+
+  async function addPost() {
+    const newPostObj = getAddPostContent();
+    const responsePost = await fetchData(`http://localhost:3000/posts/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPostObj),
+    });
+    console.log("responsePost: ", responsePost);
+
+    setPosts((prevPosts) => [...prevPosts, responsePost]);
+  }
+
+  async function changePost(postId) {
+    const newPostObj = getAddPostContent();
+    const responsePost = await fetchData(
+      `http://localhost:3000/posts/${postId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPostObj),
+      }
     );
-  });
+    console.log("responsePost: ", responsePost);
+
+    setPosts((prevPosts) => {
+      let newPosts = [...prevPosts];
+      const postIndex = prevPosts.findIndex(
+        (post) => post.id === responsePost.id
+      );
+      newPosts[postIndex] = responsePost;
+      return newPosts;
+    });
+  }
+
+  function getAddPostContent() {
+    const postTitle = prompt("please enter your post title");
+    const postBody = prompt("please enter your post body");
+    const newPost = {
+      title: postTitle,
+      body: postBody,
+      userId: id,
+    };
+    return newPost;
+  }
+
+  const postsDisplay =
+    posts &&
+    posts.map((post) => {
+      return (
+        <div key={post.id}>
+          <button
+            onClick={() => {
+              setShowPost(true);
+              navigate(`/home/${id}/posts/${post.id}`);
+            }}
+          >
+            show post
+          </button>
+          <button
+            onClick={() => {
+              setShowPost(false);
+              navigate(`/home/${id}/posts`);
+            }}
+          >
+            hide post
+          </button>
+          <button
+            onClick={async () => {
+              await changePost(post.id);
+            }}
+          >
+            update post
+          </button>
+          <button
+            onClick={async () => {
+              await deletePost(post.id);
+            }}
+          >
+            delete post
+          </button>
+          <h4>id: {post?.id}</h4>
+          <h4>title: {post?.title}</h4>
+          {showPost && (
+            <>
+              <h4>userId: {post?.userId}</h4>
+              <h4>body: {post?.body}</h4>
+              <button
+                onClick={() => {
+                  setShowComments(true);
+                  navigate(`/home/${id}/posts/${post.id}/comments`);
+                }}
+              >
+                show comments
+              </button>
+              <button
+                onClick={() => {
+                  setShowComments(false);
+                  navigate(`/home/${id}/posts/${post.id}`);
+                }}
+              >
+                hide comments
+              </button>
+            </>
+          )}
+        </div>
+      );
+    });
   return (
     <div>
       <section>
         <h2>Your Posts</h2>
+        <button onClick={addPost}>add post</button>
         <div>
-          {posts.length > 0 ? (
+          {posts && posts.length > 0 ? (
             <section>{postsDisplay}</section>
           ) : (
             <h2>Loading...</h2>
